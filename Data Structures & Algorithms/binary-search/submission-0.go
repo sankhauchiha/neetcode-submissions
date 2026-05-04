939aa7a5dfc6f641d580ce16d1992c894646d7b7
sankhauchiha <142609530+sankhauchiha@users.noreply.github.com>
2026-05-04 23:43:29 +0530

    Add: binary-search - submission-0

---
+++ Data Structures & Algorithms/binary-search/submission-0.go
@@ -0,0 +1,14 @@
+func search(nums []int, target int) int {
+    i , j := 0, len(nums) - 1
+    for i <= j {
+        mid := i + (j-i)/2
+        if nums[mid] > target {
+            j = mid - 1
+        }else if nums[mid] < target {
+            i = mid + 1
+        }else{
+            return mid
+        }
+    }
+    return -1
+}
